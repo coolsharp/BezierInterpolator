17eca2041564b6f1e200453ac438cb8e3d2b8854
coolsharp <coolsharp@gmail.com>
2016-07-26 23:49:37 +0900

    add scale job

--- app/src/main/java/net/xamous/bezierinterpolatordemo/SampleActivity.java
+++ app/src/main/java/net/xamous/bezierinterpolatordemo/SampleActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v4.view.animation.PathInterpolatorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
 import android.widget.CompoundButton;
@@ -29,23 +30,29 @@ public class SampleActivity extends AppCompatActivity implements BezierView.OnCu
     protected SeekBar mSeekBar;
     protected TextView mDurationText;
     protected TextView mParamText;
-    protected Switch mSwitch;
 
-    protected ValueAnimator mActorAnim;
-    protected ValueAnimator mIndicatorAnim;
-    protected AnimatorSet mAnimatorSet;
+    protected SeekBar seekBarScaleStart;
+    protected TextView textViewScaleStart;
+
+    protected SeekBar seekBarScaleEnd;
+    protected TextView textViewScaleEnd;
+
+    //    protected ValueAnimator mActorAnim;
     protected Interpolator mInterpolator;
     protected long mDuration;
+
+    protected float startScale;
+    protected float endScale;
 
     protected final static float DEFAULT_X1 = 0;
     protected final static float DEFAULT_Y1 = .5f;
     protected final static float DEFAULT_X2 = .5f;
     protected final static float DEFAULT_Y2 = 1;
-    protected final static int DEFAULT_DURATION = 1500;
+    protected final static int DEFAULT_DURATION = 500;
 
     protected final static int REPEAT_DELAY = 500;
     protected final static int MIN_DURATION = 100;
-    protected final static int MAX_DURATION = 10000;
+    protected final static int MAX_DURATION = 5000;
 
 
 
@@ -63,12 +70,55 @@ public class SampleActivity extends AppCompatActivity implements BezierView.OnCu
         mSeekBar.setMax(MAX_DURATION - MIN_DURATION);
         mSeekBar.setProgress(DEFAULT_DURATION - MIN_DURATION);
 
+        textViewScaleStart = (TextView) findViewById(R.id.scaleStart);
+
+        seekBarScaleStart = (SeekBar) findViewById(R.id.seekBarscaleStart);
+        seekBarScaleStart.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textViewScaleStart.setText("시작 크기 : " + progress + "%");
+                startScale = (float)progress / 100f;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBarScaleStart.setMax(200);
+        seekBarScaleStart.setProgress(50);
+
+        textViewScaleEnd = (TextView) findViewById(R.id.scaleEnd);
+
+        seekBarScaleEnd = (SeekBar) findViewById(R.id.seekBarScaleEnd);
+        seekBarScaleEnd.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textViewScaleEnd.setText("종료 크기 : " + progress + "%");
+                endScale = (float)progress / 100f;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBarScaleEnd.setMax(200);
+        seekBarScaleEnd.setProgress(150);
+
         mBezierView = (BezierView) findViewById(R.id.bezierView);
         mBezierView.setCurve(DEFAULT_X1, DEFAULT_Y1, DEFAULT_X2, DEFAULT_Y2);
         mBezierView.setOnCurveChangedListener(this);
-
-        mSwitch = (Switch) findViewById(R.id.toggle);
-        mSwitch.setOnCheckedChangeListener(this);
 
         onCurveChanged(DEFAULT_X1, DEFAULT_Y1, DEFAULT_X2, DEFAULT_Y2);
 
@@ -76,29 +126,27 @@ public class SampleActivity extends AppCompatActivity implements BezierView.OnCu
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
-        mActorAnim = ObjectAnimator.ofFloat(mActor, "translationX", actorWidth,
-                metrics.widthPixels - actorWidth * 2);
-        mActorAnim.setInterpolator(mInterpolator);
-
-        mIndicatorAnim = ObjectAnimator.ofFloat(mBezierView, "indicatorPos", 0, 1);
-        mIndicatorAnim.setInterpolator(new LinearInterpolator());
-
-        mAnimatorSet = new AnimatorSet();
-        mAnimatorSet.playTogether(mActorAnim, mIndicatorAnim);
-
-
-        mAnimatorSet.addListener(new AnimatorListenerAdapter() {
+        mActor.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onAnimationEnd(Animator animation) {
-                mActor.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        startAnimation();
-                    }
-                }, REPEAT_DELAY);
+            public void onClick(View v) {
+                ObjectAnimator scaleDownX;
+                ObjectAnimator scaleDownY;
+                ValueAnimator mIndicatorAnim;
+                AnimatorSet mAnimatorSet;
+                scaleDownX = ObjectAnimator.ofFloat(mActor, "scaleX", startScale, endScale);
+                scaleDownX.setInterpolator(mInterpolator);
+                scaleDownY = ObjectAnimator.ofFloat(mActor, "scaleY", startScale, endScale);
+                scaleDownY.setInterpolator(mInterpolator);
+
+                mIndicatorAnim = ObjectAnimator.ofFloat(mBezierView, "indicatorPos", 0, 1);
+                mIndicatorAnim.setInterpolator(new LinearInterpolator());
+
+                mAnimatorSet = new AnimatorSet();
+                mAnimatorSet.playTogether(scaleDownX, scaleDownY, mIndicatorAnim);
+                mAnimatorSet.setDuration(mDuration);
+                mAnimatorSet.start();
             }
         });
-        startAnimation();
     }
 
     @Override
@@ -108,18 +156,12 @@ public class SampleActivity extends AppCompatActivity implements BezierView.OnCu
     }
 
     private Interpolator createInterpolator(float x1, float y1, float x2, float y2) {
-        return mSwitch.isChecked() ? new BezierInterpolator(x1, y1, x2, y2) : PathInterpolatorCompat.create(x1, y1, x2, y2);
+        return PathInterpolatorCompat.create(x1, y1, x2, y2);
     }
 
     private void setDuration(int duration) {
         mDuration = duration;
         mDurationText.setText(String.format(getString(R.string.duration), mDuration));
-    }
-
-    private void startAnimation() {
-        mActorAnim.setInterpolator(mInterpolator);
-        mAnimatorSet.setDuration(mDuration);
-        mAnimatorSet.start();
     }
 
 
